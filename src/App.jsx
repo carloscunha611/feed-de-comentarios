27c5492984e4b870684521596063f74452c85293
Carlos <carloscunha611@hotmail.com>
2023-08-09 21:32:30 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react"
+
+export default function App() {
+  const [comments, setComments] = useState([])
+  const [author, setAuthor] = useState('')
+  const [content, setContent] = useState('')
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault()
+
+    const newComment = {
+      id: Math.floor(Math.random() * 1000000),
+      author: author,
+      content: content,
+      createdAt: new Date()
+    }
+
+    setComments((state) => [newComment, ...state])
+    setAuthor('')
+    setContent('')
+  }
+  return (
+   <div id="app">
+    <h2>Seção de comentários</h2>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="author">Email</label>
+      <input 
+        type="email" 
+        id="author" 
+        placeholder="Escreva seu email aqui."
+        required
+        value={author}
+        onChange={(ev) => setAuthor(ev.target.value)}
+      />
+      <label htmlFor="content">Comentário</label>
+      <textarea 
+        id="content" 
+        cols="30" rows="6" 
+        placeholder="Escreva seu comentário aqui."
+        value={content}
+        onChange={(ev) => setContent(ev.target.value)}  
+      ></textarea>
+      <button type="submit">Enviar comentário</button>
+    </form>
+    <hr />
+    <section id="comments">
+      {comments.length > 0
+        ? (
+            comments.map((comment) => (
+              <div key={comment.id}>
+                <h3>{comment.author}</h3>
+                <span>Em {comment.createdAt.toLocaleString()}</span>
+                <p>{comment.content}</p>
+              </div>
+            ))
+          ) : (
+            <p>Seja o primeiro a comentar!</p>
+          )
+      }
+    </section>
+   </div>
+  )
+}
+
